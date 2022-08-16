8481e9a80063af4bcee413388ea70f9804f3d54a
Vikram Reddy <gvreddy04@hotmail.com>
2022-08-16 22:38:18 +0530

    Added unit tests.

--- WebUI.Test/Pages/Students/StudentsTest.cs
+++ WebUI.Test/Pages/Students/StudentsTest.cs
@@ -31,4 +31,33 @@ public class StudentsTest : TestContext
         var h1 = cut.Find("h1");
         h1.MarkupMatches("<h1 diff:ignoreAttributes>Students</h1>");
     }
+
+    [Fact]
+    public void StudentsComponentRendersCorrectly_02()
+    {
+        // Arrange
+        List<Student> students = new()
+        {
+            new Student
+            {
+                StudentId = 1,
+                StudentName = "Test Student",
+                GaurdianName = "Test Gaurdian",
+                Email = "test@email.com",
+                PhoneNumber = "9988776655",
+                Gender = 1,
+                CountryCode = 1,
+                DOB = new DateTime(1990, 1, 1),
+                Oid = Guid.NewGuid()
+            }
+        };
+        _studentServiceMock.Setup(x => x.GetStudentsAsync()).ReturnsAsync(students);
+
+        // Act
+        var cut = this.RenderComponent<WebUI.Pages.Students.Students>();
+
+        // Assert
+        var rows = cut.FindAll("tbody tr");
+        Assert.Equal(1, rows.Count);
+    }
 }
