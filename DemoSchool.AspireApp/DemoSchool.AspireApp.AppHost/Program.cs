06e95741f753f56028784a11bdfaaf8c1df04c7f
Vikram Reddy <gvreddy04@gmail.com>
2023-12-17 17:57:12 +0530

    UI project included.

--- DemoSchool.AspireApp/DemoSchool.AspireApp.AppHost/Program.cs
+++ DemoSchool.AspireApp/DemoSchool.AspireApp.AppHost/Program.cs
@@ -1,3 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.DemoSchool_WebAPI>("demoschool.webapi");
+
+builder.AddProject<Projects.DemoSchool_WebUI>("demoschool.webui");
+
 builder.Build().Run();
--- DemoSchool.WebAPI/Program.cs
+++ DemoSchool.WebAPI/Program.cs
@@ -1,4 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
 {
     // Add services to the container.
     builder.Services.AddApplicationServices(builder.Configuration);
@@ -27,6 +29,8 @@ var builder = WebApplication.CreateBuilder(args);
 }
 
 var app = builder.Build();
+
+app.MapDefaultEndpoints();
 {
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
